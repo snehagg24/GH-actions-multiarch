229e698fc9465dc3e54dd109238bb0532c2c1c1c
Sneha Kanekar1 <sneha.kanekar1@ibm.com>
2022-08-09 15:30:48 +0530

    set env vars

--- create-multi-arch.sh
+++ create-multi-arch.sh
@@ -2,11 +2,11 @@
 
 export DOCKER_CLI_EXPERIMENTAL=enabled
 
-docker manifest create ${{ secrets.QUAY_REPO }}/test-gitlab:latest \
-${{ secrets.QUAY_REPO }}/test-gh-action-multiarch:latest-x86_64 ${{ secrets.QUAY_REPO }}/test-gh-action-multiarch:latest-ppc64le
+docker manifest create $QUAY_REPO/test-gitlab:latest \
+$QUAY_REPO/test-gh-action-multiarch:latest-x86_64 $QUAY_REPO/test-gh-action-multiarch:latest-ppc64le
 
-docker manifest inspect ${{ secrets.QUAY_REPO }}/test-gh-action-multiarch:latest
+docker manifest inspect $QUAY_REPO/test-gh-action-multiarch:latest
 
-docker login quay.io -u ${{ secrets.ROBOT_USER }} -p ${{ secrets.ROBOT_TOKEN }}
+docker login quay.io -u $ROBOT_USER -p $ROBOT_TOKEN
 
-docker manifest push ${{ secrets.QUAY_REPO }}/test-gh-action-multiarch:latest
+docker manifest push $QUAY_REPO/test-gh-action-multiarch:latest
