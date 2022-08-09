f36a51215a32345b276078e5f2cf842d7cdf502d
Sneha Kanekar1 <sneha.kanekar1@ibm.com>
2022-08-09 15:57:10 +0530

    pass arg to build script

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 ARCH=$(uname -m)
+if (( $# == 3 ))
+then
+    QUAY_REPO=$1
+    ROBOT_USER=$2
+	ROBOT_TOKEN=$3
+fi
 
 LD_DEBUG=libs docker build -t container/$ARCH -f ./$ARCH/Dockerfile .
 docker tag container/$ARCH $QUAY_REPO/test-gh-action-multiarch:latest-$ARCH
