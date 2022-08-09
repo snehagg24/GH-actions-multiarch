1e65a513e3d5bd0f49d8a5caa196d0fa436dd4cc
Sneha Kanekar1 <sneha.kanekar1@ibm.com>
2022-08-09 15:33:05 +0530

    fix quay auth

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ ARCH=$(uname -m)
 
 LD_DEBUG=libs docker build -t container/$ARCH -f ./$ARCH/Dockerfile .
 docker tag container/$ARCH $QUAY_REPO/test-gh-action-multiarch:latest-$ARCH
-docker login quay.io -u ROBOT_USER -p $ROBOT_TOKEN
-docker push $QUAY_REPO/test-gitlab:latest-$ARCH
+docker login quay.io -u $ROBOT_USER -p $ROBOT_TOKEN
+docker push $QUAY_REPO/test-gh-action-multiarch:latest-$ARCH
 echo "DONE!"
